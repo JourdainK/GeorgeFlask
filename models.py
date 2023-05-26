a333537aae2fc8301b139b22f8120dda5c13af45
JourdainK <K.jourdain@condorcet.be>
2023-05-27 00:06:35 +0200

    sort prods + vues

--- models.py
+++ models.py
@@ -1,7 +1,7 @@
 from . import app, db
 from flask_sqlalchemy import SQLAlchemy
 
-class produits(db.Model):
+class produit(db.Model):
     id_produit = db.Column(db.Integer,primary_key=True)
     nom_produit = db.Column(db.String(25),nullable=False)
     description = db.Column(db.String(200),nullable=True)
@@ -22,21 +22,28 @@ class categorie(db.Model):
         return f'{self.id_categorie} : {self.nom_categorie} : {self.pic}'
 
 
-class attribut(db.Model):
+
+class vue_attributs(db.Model):
     id_attribut = db.Column(db.Integer,primary_key=True)
     nom_attribut = db.Column(db.String(30),nullable=False)
-
-    def __repr__(self):
-        return f'{self.id_attribut} : {self.nom_attribut}'
-
-class produits_attributs(db.Model):
-    id_produit = db.Column(db.Integer,primary_key=True)
-    id_attribut = db.Column(db.Integer,primary_key=True)
     valeur = db.Column(db.String(30),nullable=False)
+    id_produit = db.Column(db.Integer,primary_key=True)
 
     def __repr__(self):
         return f'{self.id_produit} : {self.id_attribut} : {self.valeur}'
 
+
+class vue_produits_categories(db.Model):
+    id_produit = db.Column(db.Integer,primary_key=True)
+    nom_produit = db.Column(db.String(25),nullable=False)
+    description = db.Column(db.String(200),nullable=True)
+    prix = db.Column(db.Float(10),nullable=True)
+    photo = db.Column(db.String(30),nullable=True,default='default.jpg')
+    id_categorie = db.Column(db.Integer)
+    nom_categorie = db.Column(db.String(30),nullable=False)
+    pic = db.Column(db.String(30),nullable=True,default='default.jpg')
+    def __repr__(self):
+        return f'{self.id_produit} : {self.nom_produit} : {self.description} : {self.prix} : {self.photo} : {self.id_categorie} : {self.nom_categorie} : {self.image}'
 
 class vue_dvds(db.Model):
     id_produit = db.Column(db.Integer,primary_key=True)
--- routes.py
+++ routes.py
@@ -1,12 +1,21 @@
 from flask import render_template, redirect, url_for, request
 from . import app, models
 
+
 @app.route('/')
 def accueil():
     liste_categories = models.categorie.query.all()
-    return render_template('accueil.html', title='George Carlin Website', liste_cat=liste_categories,typecat=type(liste_categories))
+    return render_template('accueil.html', title='George Carlin Website', liste_cat=liste_categories, typecat=type(liste_categories))
 
-@app.route('/categorie')
-def categorie():
-    liste_categories = models.categorie.query.all()
-    return render_template('categorie.html', title='George Carlin Website - Produits', liste_cat=liste_categories, typecat=type(liste_categories))
+
+
+
+@app.route('/produits_categorie')
+def produits_categorie():
+    id_categ = request.args.get('id_categorie')
+    liste_produits = models.vue_produits_categories.query.filter_by(id_categorie=id_categ).all()
+    liste_attributs = models.vue_attributs.query.all()
+
+    return render_template('produits_categorie.html', title='Nos produits', produits=liste_produits,  typeprod=type(liste_produits) , attributs=liste_attributs, typeatr=type(liste_attributs))
+
+
